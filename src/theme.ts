4348ed6987e7b56891ac0b70374b5740652a02fe
johanCarvajalRPA <98964064+johanCarvajalRPA@users.noreply.github.com>
2024-07-31 15:06:34 -0500

    first commit

---
+++ src/theme.ts
@@ -0,0 +1,35 @@
+'use client';
+import { Roboto } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      // Purple and green play nicely together.
+      main: "#FA4616",
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: "#003B49",
+    },
+    error: {
+      // This is green.A700 as hex.
+      main: "#DC0000",
+    },
+    background: {
+      default: "#f0f0f0",
+    },
+  },
+  typography: {
+    fontFamily: "Roboto, sans-serif",
+  },
+});
+
+export default theme;
